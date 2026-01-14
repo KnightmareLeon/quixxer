75adf33772b5f5ed1d63cdf0e12c0930e4aa991d
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-01-14 10:56:31 +0800

    build: added javafx dependecies

--- src/main/java/io/github/knightmareleon/App.java
+++ src/main/java/io/github/knightmareleon/App.java
@@ -1,7 +1,21 @@
 package io.github.knightmareleon;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX 25!");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setTitle("Quixxer JavaFX Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, Maven + VS Code!");
+        launch(args);
     }
 }
