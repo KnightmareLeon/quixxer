384aa008490f8009df4b2d07e41c085d6da1a6f0
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-02-06 21:11:07 +0800

    feat(test): add content for test tab

--- src/main/java/io/github/knightmareleon/features/test/TestController.java
+++ src/main/java/io/github/knightmareleon/features/test/TestController.java
@@ -3,6 +3,13 @@ package io.github.knightmareleon.features.test;
 import javafx.fxml.FXML;
 
 public class TestController {
+/*     @FXML
+    private ScrollPane scrollPane;
+
+    public TestController() {
+
+    }*/
+
     @FXML
     public void initialize() {
         System.out.println("Test loaded");
