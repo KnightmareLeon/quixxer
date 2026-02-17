468fa10a70e8436d908486fadb47da70fff367ed
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-02-17 21:07:32 +0800

    chore(app): set window to maximized size when starting

--- src/main/java/io/github/knightmareleon/App.java
+++ src/main/java/io/github/knightmareleon/App.java
@@ -1,7 +1,5 @@
 package io.github.knightmareleon;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import java.sql.Connection;
 
 import io.github.knightmareleon.database.DatabaseManager;
@@ -17,6 +15,7 @@ public class App extends Application {
     public void start(Stage stage) throws Exception{
 
         Connection connection = DatabaseManager.getConnection();
+
         Font.loadFont(getClass().getResourceAsStream("/io/github/knightmareleon/fonts/Inter_24pt-ExtraLight.ttf"), 16);
         Font.loadFont(getClass().getResourceAsStream("/io/github/knightmareleon/fonts/Inter-VariableFont_opsz,wght.ttf"), 16);
 
@@ -26,11 +25,8 @@ public class App extends Application {
 
         Parent root = rootLoader.load();
 
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        double width = screenSize.getWidth();
-        double height = screenSize.getHeight();
+        Scene scene = new Scene(root);
 
-        Scene scene = new Scene(root, width, height - 100);
         scene.getStylesheets().add(
             getClass().getResource("/io/github/knightmareleon/css/base.css").toExternalForm()
         );
@@ -39,6 +35,7 @@ public class App extends Application {
         );
 
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.setTitle("Quixxer");
         stage.show();
     }
