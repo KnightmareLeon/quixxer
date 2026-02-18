b63c59ab96eac880f361cd3b37a499dc0c34d89f
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-02-18 11:11:57 +0800

    feat(database): add tables for standard and true or false questions along with indexes

--- src/main/resources/io/github/knightmareleon/database/schema.sql
+++ src/main/resources/io/github/knightmareleon/database/schema.sql
@@ -8,3 +8,29 @@ CREATE TABLE IF NOT EXISTS study_set (
     total_takes INTEGER NOT NULL DEFAULT 0,
     UNIQUE(title, subject)
 );
+
+CREATE TABLE IF NOT EXISTS standard_question (
+    id INTEGER PRIMARY KEY,
+    type INTEGER NOT NULL CHECK (type >= 0),
+    description TEXT NOT NULL,
+    set_id INTEGER NOT NULL,
+    FOREIGN KEY (set_id)
+        REFERENCES study_set(id)
+        ON DELETE CASCADE
+);
+
+CREATE INDEX idx_std_question_set_id_id
+ON standard_question(set_id, id);
+
+CREATE TABLE IF NOT EXISTS tof_question (
+    id INTEGER PRIMARY KEY,
+    description TEXT NOT NULL,
+    is_true INTEGER NOT NULL CHECK (is_true IN (0,1)),
+    set_id INTEGER NOT NULL,
+    FOREIGN KEY (set_id) 
+        REFERENCES study_set(id) 
+        on DELETE CASCADE
+);
+
+CREATE INDEX idx_tof_question_set_id_id
+ON tof_question(set_id, id);
