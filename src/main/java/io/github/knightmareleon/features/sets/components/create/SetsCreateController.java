ad6a634378d94d559632ef8bee9a0898a500bfe4
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-02-20 10:38:24 +0800

    feat(sets): add items to subject picker; fix formatting in creat set content area

--- src/main/java/io/github/knightmareleon/features/sets/components/create/SetsCreateController.java
+++ src/main/java/io/github/knightmareleon/features/sets/components/create/SetsCreateController.java
@@ -2,15 +2,35 @@ package io.github.knightmareleon.features.sets.components.create;
 
 import io.github.knightmareleon.features.sets.components.SetsNavigator;
 import io.github.knightmareleon.features.sets.components.SetsPage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
 public class SetsCreateController implements SetsPage{
 
     private SetsNavigator navigator;
 
+    @FXML
+    private ComboBox<String> subjectPicker;
+    private ObservableList<String> subjects = FXCollections.observableArrayList(
+        "Computer Science",
+        "Physics",
+        "Biology",
+        "Chemistry",
+        "Geology",
+        "History",
+        "Language"
+    );
+
     @Override
     public void setSetsNavigator(SetsNavigator navigator) {
         this.navigator = navigator;
+    }
+
+    @FXML
+    public void initialize(){
+        this.subjectPicker.setItems(this.subjects);
     }
 
     @FXML
