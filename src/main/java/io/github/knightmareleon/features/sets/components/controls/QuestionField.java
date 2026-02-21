a752611427242679f2de7637d54f4e274bf2a1d5
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-02-21 17:40:18 +0800

    feat(sets): change choices on question field based on chosen question type; style: change font and text color on fields

--- src/main/java/io/github/knightmareleon/features/sets/components/controls/QuestionField.java
+++ src/main/java/io/github/knightmareleon/features/sets/components/controls/QuestionField.java
@@ -11,9 +11,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -47,8 +50,40 @@ public class QuestionField extends VBox{
     }
     @FXML
     public void initialize(){
+        this.addChoiceButton.setMaxWidth(Double.MAX_VALUE);
+        this.qTypePicker.setMinHeight(48);
         this.qTypePicker.setItems(this.qTypes);
-        this.addChoice();
+
+        this.qTypePicker.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
+            if(newVal.equals(oldVal)) {
+                return;
+            }
+
+            choices.getChildren().clear();
+            choiceRows.clear();
+
+            if(newVal.equals("Identification") || newVal.equals("Enumeration")){
+                addChoiceButton.setVisible(true);
+                this.addChoice();
+            } else {
+                addChoiceButton.setVisible(false);
+                ToggleGroup trueOrFalse = new ToggleGroup();
+                RadioButton trueButton = new RadioButton("True");
+                RadioButton falseButton = new RadioButton("False");
+
+                trueButton.setToggleGroup(trueOrFalse);
+                falseButton.setToggleGroup(trueOrFalse);
+                trueButton.setSelected(true);
+
+                trueButton.getStyleClass().add("standard-font");
+                falseButton.getStyleClass().add("standard-font");
+
+                choices.getChildren().addAll(trueButton, falseButton);
+            }
+
+        });
+
+        this.qTypePicker.setValue("Identification");
     }
 
     @FXML
@@ -65,10 +100,25 @@ public class QuestionField extends VBox{
         row.setAlignment(Pos.CENTER);
 
         choiceField.setMinHeight(48);
-        choiceField.getStyleClass().add("border-radius-15");
-
+        choiceField.getStyleClass().addAll("border-radius-15","standard-font");
+        choiceField.setStyle("-fx-text-fill: black !important");
         HBox.setHgrow(choiceField, Priority.ALWAYS);
+
+        deleteChoiceField.setMinHeight(48);
+        deleteChoiceField.setMinWidth(48);
         deleteChoiceField.getStyleClass().add("icon-button-base-bg");
+
+        if(qTypePicker.getValue().equals("Identification")){
+            CheckBox checkBox = new CheckBox();
+
+            row.getChildren().add(checkBox);
+
+            if (choices.getChildren().isEmpty()){
+                checkBox.setSelected(true);
+            }
+        }
+
+        row.setSpacing(12);
         row.getChildren().addAll(choiceField, deleteChoiceField);
         choiceRows.add(row);
 
@@ -82,5 +132,11 @@ public class QuestionField extends VBox{
         choices.getChildren().add(row);
     }
 
+    public String getQuestion(){
+        return this.question.getText();
+    }
 
+    public String getType(){
+        return this.qTypePicker.getValue();
+    }
 }
