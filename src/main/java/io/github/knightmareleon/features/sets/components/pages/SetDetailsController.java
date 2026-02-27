fa3a751b6cce1106d12297d6c6cd4309a850e47c
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-02-27 13:18:37 +0800

    feat(sets): add set details header and actual details from study set

--- src/main/java/io/github/knightmareleon/features/sets/components/pages/SetDetailsController.java
+++ src/main/java/io/github/knightmareleon/features/sets/components/pages/SetDetailsController.java
@@ -5,12 +5,18 @@ import io.github.knightmareleon.features.sets.components.SetsNavigator;
 import io.github.knightmareleon.features.sets.components.SetsPage;
 import io.github.knightmareleon.shared.models.StudySet;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class SetDetailsController implements SetsPage {
 
     private final SetsService setsService;
     private SetsNavigator navigator;
     private StudySet studySet;
+
+    @FXML private Label setName;
+    @FXML private Label subjectLabel;
+    @FXML private Label createdOnLabel;
+    @FXML private Label lastTakeOnLabel;
 
     public SetDetailsController(SetsService setsService){
         this.setsService = setsService;
@@ -24,11 +30,16 @@ public class SetDetailsController implements SetsPage {
     public void setStudySet(StudySet studySet){
         System.out.println("Retrieved study set:\n" + studySet );
         this.studySet = studySet;
+        this.setName.setText(this.studySet.getTitle());
+        this.subjectLabel.setText(this.studySet.getSubject());
+        this.createdOnLabel.setText(this.studySet.getDateCreatedOn().toString());
+        this.lastTakeOnLabel.setText(this.studySet.getDataLastTakeOn() == null ? 
+            "Not yet taken." : this.studySet.getDataLastTakeOn().toString());
     }
 
     @FXML
     public void initialize(){
-
+        
     }
 
     @FXML
