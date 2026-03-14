b8765d88967dc232fb309c42659107f90d55c749
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-03-14 14:52:47 +0800

    feat(models): add test data model

---
+++ src/main/java/io/github/knightmareleon/shared/models/TestData.java
@@ -0,0 +1,57 @@
+package io.github.knightmareleon.shared.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.knightmareleon.features.test.components.constants.TestType;
+
+public class TestData {
+    private final TestType type;
+    private final StudySet studySet;
+
+    private List<Question> questionsUsed;
+
+    private final boolean timed;
+    private final String timeCategory;
+
+    private int score = 0;
+
+    public TestData(TestType type, StudySet studySet, int totalQuestionsUsed,
+                    boolean timed, String timeCategory, boolean shuffled){
+        this.type = type;
+        this.studySet = studySet;
+        this.timed = timed;
+        this.timeCategory = timeCategory;
+        this.setQuestionsUsed(this.studySet.getQuestions(), shuffled, totalQuestionsUsed);
+    }
+
+    private void setQuestionsUsed(List<Question> questionsList, boolean shuffled, int totalToBeUsed){
+        List<Question> questionsToBeUsed = new ArrayList<>(questionsList);
+        if(shuffled) java.util.Collections.shuffle(questionsToBeUsed);
+        this.questionsUsed = questionsToBeUsed.subList(0, totalToBeUsed);
+    }
+
+    public TestType getType(){
+        return this.type;
+    }
+
+    public List<Question> getQuestionsUsed(){
+        return this.questionsUsed;
+    }
+
+    public boolean isTimed(){
+        return this.timed;
+    }
+
+    public String getTimeCategory(){
+        return this.timeCategory;
+    }
+
+    public void incrementScore(){
+        this.score++;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+}
