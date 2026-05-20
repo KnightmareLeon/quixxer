bf32ea291ab891cfd530461d84923e2f3993b245
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-05-20 15:15:30 +0800

    feat(dashboard): add transition

--- src/main/java/io/github/knightmareleon/features/dashboard/DashboardController.java
+++ src/main/java/io/github/knightmareleon/features/dashboard/DashboardController.java
@@ -1,11 +1,14 @@
 package io.github.knightmareleon.features.dashboard;
 
 import io.github.knightmareleon.shared.infrastructure.AppContext;
+import io.github.knightmareleon.shared.utils.Transitions;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 public class DashboardController {
 
     private final AppContext context;
+    @FXML private VBox mainContainer;
 
     public DashboardController(AppContext context){
         this.context = context;
@@ -14,5 +17,6 @@ public class DashboardController {
     @FXML
     public void initialize() {
         System.out.println("Dashboard loaded");
+        Transitions.standardFadeTransition(this.mainContainer);
     }
 }
