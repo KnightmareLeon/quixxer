caf8079913e024ff0e91e8af5e3222e11bf677fe
KnightmareLeon <leonardjohncorpuz20@gmail.com>
2026-05-22 23:29:16 +0800

    feat(dashboard): show total study sets

--- src/main/java/io/github/knightmareleon/features/dashboard/DashboardController.java
+++ src/main/java/io/github/knightmareleon/features/dashboard/DashboardController.java
@@ -3,12 +3,15 @@ package io.github.knightmareleon.features.dashboard;
 import io.github.knightmareleon.shared.infrastructure.AppContext;
 import io.github.knightmareleon.shared.utils.Transitions;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class DashboardController {
 
     private final DashboardService dbService;
+
     @FXML private VBox mainContainer;
+    @FXML private Label totalSetsLabel;
 
     public DashboardController(AppContext context){
         this.dbService = context.getDBService();
@@ -18,6 +21,7 @@ public class DashboardController {
     @FXML
     public void initialize() {
         System.out.println("Dashboard loaded");
+        this.totalSetsLabel.setText("" + dbService.getTotalSets());
         Transitions.standardFadeTransition(this.mainContainer);
     }
 }
